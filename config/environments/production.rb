01d455684f07da09027e35d27dbe145b9ea50bbd
ubershibs <luke.walker@gmail.coml>
2016-03-12 12:07:04 -0500

    Fixing configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_protocol => 'http',
-  :url = ':s3_domain_url',
-    :path = '/:class/:attachment/:id_partition/:style/:filename',
-    :s3_host_name = 's3-us-west-2.amazonaws.com',
+  :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
